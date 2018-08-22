2f04672c2fd3bc3ded17948a27c7d1bd685c36cf
datdta95 <datdta95@gmail.com>
2018-08-22 11:18:57 +0700

    Tạo đối tượng Rectangle với 2 phương thức lấy được kích thước chiều dài chiều rộng

---
+++ src/main/java/tiendat/thinkpad/rectangle/Rectangle.java
@@ -0,0 +1,22 @@
+package tiendat.thinkpad.rectangle;
+
+public class Rectangle {
+    double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public  double getWidth() {
+        return this.width ;
+    }
+
+    public  double getHeight() {
+        return this.height ;
+    }
+
+}
